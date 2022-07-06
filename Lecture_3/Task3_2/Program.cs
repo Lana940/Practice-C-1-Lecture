48ca6635c5d837e1ce90ce2d4b1793de0e8d66ca
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-06 22:30:08 +0300

    Lecture 3

---
+++ Lecture_3/Task3_2/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Упорядочить массив
+
+int [] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int [] array)  // этап печати массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+
+    Console.WriteLine();
+}
+
+void SelectionSort( int [] array) // 2 етап сортировки
+{
+
+    for (int i = 0; i < array.Length - 1; i++)   //array.Length -1 - искуственно отнимаем 1, тк дальше j = i +1 и оно даст общее кол-во элементов
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)     //этот блок кода ищет максм елемент, 
+        {
+            if(array[j] < array[minPosition]) minPosition = j;   
+
+        }
+            
+        int temporary = arr[i];           // а тут происходит обмен, как в блок схемах последнего урока
+        array [i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
---
+++ Lecture_3/task_1/Program.cs
@@ -0,0 +1,80 @@
+﻿// Вид 1 - ничего не принимают, ничего не возвращают
+
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+Method1(); // vyzov methoda
+
+// Вид 2  - принимает что-то, но ничего не возвращает
+
+void Method2(string msg)  // какие то аргументы метода
+{
+    Console.WriteLine(msg);
+}
+Method2("текст сообщения");
+
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+Method21(msg: "текст", count: 4);  //именованные аргументы
+
+//Вид 3 что то возвращает, но ничего не принимают
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//Вид 4  что-то принимает и что-то возвращает 
+
+// string Method4(int count, string text)    //( есть строка, которую надо напечатать count раз)
+// {
+//     int i = 0;
+//     string result = String.Empty;  // переменная куда кладем конечный результат, и в нее изначально нужно положить какое то значение 0 или пусто в данном случае
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "Zs");
+// Console.WriteLine(res);
+
+// то же самое с циклом for
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for( int i = 0; i < count; i ++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "zt");
+Console.WriteLine(res);
+
+//таблица умножения
+
+for( int i = 2; i <= 10; i ++)
+{
+    for ( int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = { i * j}");
+    }
+    Console.WriteLine();
+}
